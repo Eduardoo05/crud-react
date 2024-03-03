205c8df191c9746a4e825bdaaac863e5299d370a
Elvin Eduardo <franco193@hotmail.com>
2024-03-03 17:43:36 -0400

    Se crearon la data con los nombres a ocupar, se creo una estructura tipo formulario con el fin de que una vez dado click en los botones, esta data pueda ser actualizada, se estilizo el renderizado haciendo uso de bootstrap y reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Table, Button, Container, Modal, ModalHeader, ModalBody, FormGroup, ModalFooter } from 'reactstrap';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  state = {
+    //data: data,
+    modalInsertar: false,
+    form:{
+      id: '',
+      personaje: '',
+      anime: ''
+    }
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { id: 1, personaje: "Naruto", anime: "Naruto" },
+        { id: 2, personaje: "Goku", anime: "Dragon Ball" },
+        { id: 3, personaje: "Kenshin Himura", anime: "Rurouni Kenshin" },
+        { id: 4, personaje: "Monkey D. Luffy", anime: "One Piece" },
+        { id: 5, personaje: "Edward Elric", anime: "Fullmetal Alchemist: Brotherhood" },
+        { id: 6, personaje: "Seto Kaiba", anime: "Yu-Gi-Oh!" },
+      ],
+    };
+  }
+
+  handleChange=(e)=>{
+    this.setState({
+      form:{
+        ...this.state.form,
+        [e.target.name]: [e.target.value],
+      }
+    })
+  }
+
+  mostrarModalInsertar = () =>{
+    this.setState({modalInsertar: true})
+  }
+
+  ocultarModalInsertar = () =>{
+    this.setState({modalInsertar: false})
+  }
+  render() {
+    return (
+      <>
+      <Container>
+        <br />
+        <Button color="success" onClick={()=> this.mostrarModalInsertar()}>Insertar nuevo personaje</Button>
+        <br /> <br />
+
+        <Table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Personaje</th>
+              <th>Anime</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((elemento) => (
+              <tr key={elemento.id}>
+                <td>{elemento.id}</td>
+                <td>{elemento.personaje}</td>
+                <td>{elemento.anime}</td>
+                <td>
+                  <Button color="primary">Editar</Button>{"  "}
+                  <Button color="danger">Eliminar</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+
+      <Modal isOpen={this.state.modalInsertar}>
+        <ModalHeader>
+          <div>
+            <h3>Insertar registro</h3>
+          </div>
+        </ModalHeader>
+
+        <ModalBody>
+          <FormGroup>
+            <label>Id:</label>
+            <input className='form-control' readOnly type="text" value={this.state.data.length+1} />
+          </FormGroup>
+
+          <FormGroup>
+            <label>Personaje:</label>
+            <input className='form-control' name='personaje' type="text" onChange={this.handleChange} />
+          </FormGroup>
+
+          <FormGroup>
+            <label>Anime:</label>
+            <input className='form-control' name='anime' type="text" />
+          </FormGroup>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button color='primary'>Insertar</Button>
+          <Button color='danger' onClick={()=>this.ocultarModalInsertar()}>Cancelar</Button>
+        </ModalFooter>
+
+      </Modal>
+
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
